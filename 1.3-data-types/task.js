18ae7a19fc4491dd1c27d1d67b5b39f174422931
Anna Kolycheva <annakolyt@gmail.com>
2020-09-03 16:54:16 +0700

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,36 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+	let totalAmount;
+    let numberOfMonths = (date.getFullYear() - new Date().getFullYear()) * 12 - new Date().getMonth() + date.getMonth();
+    let loanBody = parseFloat(amount) - parseFloat(contribution);
+    let payment;
+
+
+    if (isNaN(parseFloat(percent))) {
+    return `Процентная ставка введина некорректно`;
+    } 
+    if (isNaN(parseFloat(contribution))) {
+    return `Некорректно введен первоначльный взнос`;
+    } 
+    if (isNaN(parseFloat(amount))) {
+    return `Некорректно введена общая стоимость`;
+    }
+    if (contribution >= amount) {
+    return 0;
+    }	
+    	
+
+    totalAmount = parseFloat(numberOfMonths * ( loanBody * (((percent / 100) / 12) + ((percent / 100) / 12) / ((Math.pow((1 + ((percent / 100) / 12)), numberOfMonths)) - 1)))).toFixed(2);
+    //console.log(`Сумма выплаты ипотеки составляет : ` + totalAmount + `руб.`)
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+	if ( name == "" || name == undefined ) {
+          name = "Аноним";
+    }
+    console.log(`Привет, мир! Меня зовут ${name}`);
+    return `Привет, мир! Меня зовут ${name}`;
 }
